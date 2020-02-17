afd57abd9e4371a386b07b2724d7d554ebf85cc4
Alexey_Serbat <alexeyserbat2517@gmail.com>
2020-02-17 14:36:06 +0300

    react-app

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Header = () =>{
+  return (
+      <div>
+        <nav>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+
+        </nav>
+      </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
